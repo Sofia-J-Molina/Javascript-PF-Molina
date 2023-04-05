bc690d0207bacb798f7db80a46c1375aa8f0e5b5
Sofia Jaime Molina <erraticamolina@gmail.com>
2023-04-04 22:20:40 -0300

    actualizacion

--- js/codigo.js
+++ js/codigo.js
@@ -5,30 +5,51 @@ while (nombreUsuario == '') {
 }
 alert('Bienvenido/a a KaosTech ' + nombreUsuario);
 
+const categorias = productos.map((el) => el.categoria);
+console.log(categorias);
+categorias.forEach((stock)=> {console.log(stock)
+});
+
 let total = 0;
 let mensaje = prompt(nombreUsuario + ' desea comprar un producto? ( si-si / no-no)');
 while (mensaje == 'si' || mensaje == 'Si') {
-    let producto = prompt('1-auriculares logitech G935\n2-auriculares JBL\n3-auriculares hp\n4-pc ar completa core i5\n5-pc gamer amd ryzen 5');
+    let producto = prompt('a-auriculares logitech G935\nb-auriculares JBL\nc-auriculares hp\nd-pc ar completa core i5\ne-pc gamer amd ryzen 5\nf-pc gamer intel Kairo core I3\ng-NOTEBOOK HP 245\nh-NOTEBOOK ACER ASPIRE 5\ni-NOTEBOOK LENOVO V15');
     switch (producto) {
-        case '1':
+        case 'a':
             alert('Agregaste auriculares logitech G935 al carro ... $ 60000');
             incrementarTotal(60000);
             break;
-        case '2':
+        case 'b':
             alert('Agregaste auriculares JBL al carro ... $ 10000');
             incrementarTotal(10000);
             break;
-        case '3':
+        case 'c':
             alert('Agregaste auriculares hp al carro ... $ 40000');
             incrementarTotal(40000);
             break;
-        case '4':
+        case 'd':
             alert('Agregaste pc ar completa core i5 al carro ... $ 130000');
             incrementarTotal(130000);
             break;
-        case '5':
+        case 'e':
             alert('Agregaste pc gamer amd ryzen 5 al carro ... $ 250000');
             incrementarTotal(250000);
+            break;
+        case 'f':
+            alert('Agregaste pc gamer intel Kairo core I3 al carro ... $ 250000');
+            incrementarTotal(373000);
+            break;
+        case 'g':
+            alert('Agregaste NOTEBOOK HP 245 al carro ... $ 250000');
+            incrementarTotal(180000);
+            break;
+        case 'h':
+            alert('Agregaste NOTEBOOK ACER ASPIRE 5 al carro ... $ 250000');
+            incrementarTotal(225000);
+            break;
+        case 'i':
+            alert('Agregaste NOTEBOOK LENOVO V15 al carro ... $ 250000');
+            incrementarTotal(188000);
             break;
         default:
             alert('Codigo inexistente');
@@ -36,9 +57,8 @@ while (mensaje == 'si' || mensaje == 'Si') {
     }
     mensaje = prompt('Desea comprar otro producto? (s-si / n-no)');
 }
-
 alert('Total de la compra $' + total);
-
+alert('Su compra ha sido realizada con exito!');
 function incrementarTotal(precio) {
     total = total + precio;
     alert('Llevas gastado $' + total);
---
+++ js/productos.js
@@ -0,0 +1,55 @@
+const productos = [{
+    id: 1,
+    nombre: "auriculares logitech G935",
+    precio: 60000,
+    categoria: "accesorios para PC"
+},
+{
+    id: 2,
+    nombre: "auriculares JBL",
+    precio: 10000,
+    categoria: "accesorios para PC"
+},
+{
+    id: 3,
+    nombre: "auriculares hp",
+    precio: 40000,
+    categoria: "accesorios para PC"
+},
+{
+    id: 4,
+    nombre: "pc ar completa core i5",
+    precio: 130000,
+    categoria: "computadoras"
+},
+{
+    id: 5,
+    nombre: "pc gamer amd ryzen 5",
+    precio: 250000,
+    categoria: "computadoras"
+},
+{
+    id: 6,
+    nombre: "PC GAMER INTEL KAIROS CORE I3",
+    precio: 373000,
+    categoria: "computadoras"
+},
+{
+    id: 7,
+    nombre: "NOTEBOOK HP 245",
+    precio: 180000,
+    categoria: "notebook"
+},
+{
+    id: 8,
+    nombre: "NOTEBOOK ACER ASPIRE 5",
+    precio: 225000,
+    categoria: "notebook"
+},
+{
+    id: 9,
+    nombre: "NOTEBOOK LENOVO V15",
+    precio: 188000,
+    categoria: "notebook"
+}
+];
